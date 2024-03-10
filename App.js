b29be17488df1c23028399b0a65c8704d69360aa
Saurabh Basak <saurabhbasak1@gmail.com>
2024-03-10 18:51:52 -0400

    App navigation init

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
-import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
-import React from "react";
+import { StatusBar } from 'expo-status-bar'
+import { Text, View } from 'react-native'
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
 
 export default function App() {
   return (
-    <View className="flex-1 justify-center items-center">
-      <Text className="font-bold">LetsLunch</Text>
-      <StatusBar />
-    </View>
-  );
+    <NavigationContainer>
+      <View className="flex-1 justify-center items-center">
+        <Text className="font-bold">LetsLunch</Text>
+        <StatusBar />
+      </View>
+    </NavigationContainer>
+  )
 }
