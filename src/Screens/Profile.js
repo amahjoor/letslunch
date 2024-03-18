adc84c81b81445319f808d5e8d3b623ea72d0749
Saurabh Basak <saurabhbasak1@gmail.com>
2024-03-18 17:25:44 -0400

    Refactored screen setup

--- src/Screens/Profile.js
+++ src/Screens/Profile.js
@@ -2,10 +2,10 @@ import { View, Text } from 'react-native'
 import { StatusBar } from 'expo-status-bar'
 import react from 'react'
 
-export default function Login() {
+export default function Profile() {
   return (
     <View className="flex-1 justify-center items-center">
-      <Text className="font-bold">Login</Text>
+      <Text className="font-bold">Settings</Text>
       <StatusBar />
     </View>
   )
